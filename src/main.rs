d437e13ea29a5b36aa2449c174dfe2340c82a1f7
Nico Feulner <nico.feulner@gmail.com>
2022-01-29 22:34:34 +0100

    Implement "Travelling Salesman activity"
    As required for my studies I changed the code to fulfil the requirements of the "Travelling Salesman activity"

--- src/main.rs
+++ src/main.rs
@@ -3,34 +3,42 @@ mod route;
 use crate::route::{Route, RouteActions};
 
 fn main() {
-    let mut route: Route = [3, 6, 4, 1, 7, 8, 2, 5];
+    // to use the greedy approach just use the greedy generator here
+    let mut route: Route = Route::generate_route();
     print!("Initial route with cost {}: ", route.calc_cost());
     route.print();
-    let n = route.len();
-    for i in 0..n {
-        let j = i % n;
-        let mut comparison: Route;
-        if j == 0 {
-            comparison = route;
-            comparison.swap(0, 1);
-        } else if j == n-1 {
-            comparison = route;
-            comparison.swap(j, n-1);
-        } else {
-            let mut first_comparison = route;
-            first_comparison.swap(j, j -1);
-            let mut second_comparison = route;
-            second_comparison.swap(j, j +1);
-            if first_comparison.is_shorter_than(&second_comparison) {
-                comparison = first_comparison;
+    {
+        let n = route.len();
+        let mut i: usize = 0;
+        let mut last_improvement: usize = 0;
+        // find improvements until there are no findings in 25 consecutive searches
+        while i - last_improvement < 25 {
+            let j = i % n;
+            let mut comparison: Route;
+            if j == 0 {
+                comparison = route;
+                comparison.swap(0, 1);
+            } else if j == n-1 {
+                comparison = route;
+                comparison.swap(j, n-1);
             } else {
-                comparison = second_comparison;
+                let mut first_comparison = route;
+                first_comparison.swap(j, j -1);
+                let mut second_comparison = route;
+                second_comparison.swap(j, j +1);
+                if first_comparison.is_shorter_than(&second_comparison) {
+                    comparison = first_comparison;
+                } else {
+                    comparison = second_comparison;
+                }
             }
-        }
-        if comparison.is_shorter_than(&route) {
-            route = comparison;
-            print!("New route in iteration {} with cost {}: ", i, route.calc_cost());
-            route.print();
+            if comparison.is_shorter_than(&route) {
+                route = comparison;
+                print!("New route in iteration {} with cost {}: ", i, route.calc_cost());
+                route.print();
+                last_improvement = i;
+            }
+            i += 1;
         }
     }
     print!("Final route with cost {}: ", route.calc_cost());
